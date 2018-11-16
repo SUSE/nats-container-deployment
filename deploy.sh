36207c5766e3efd036616f26973b4e1a9b5fedf2
Jan Dubois <jan.dubois@suse.com>
2018-11-16 15:18:28 -0800

    fissile --defaults-file option no longer supported

--- deploy.sh
+++ deploy.sh
@@ -16,13 +16,13 @@ if [ -z "$DOCKER_ORG" ]; then
   fi
 
   fissile build images --force
-  fissile build helm --auth-type rbac --defaults-file defaults.txt
+  fissile build helm --auth-type rbac
 else
   fissile build images --docker-organization $DOCKER_ORG --force
   for i in `fissile show image --docker-organization $DOCKER_ORG`; do
     docker push $i
   done
-  fissile build helm --docker-organization $DOCKER_ORG --auth-type rbac --defaults-file defaults.txt
+  fissile build helm --docker-organization $DOCKER_ORG --auth-type rbac
 fi
 
 cat > "$FISSILE_OUTPUT_DIR/Chart.yaml" << EOF
